26c22a83b1bc88ad81323b587c6b0c1ebbc83c8b
chloephuong <chloephuong09@gmail.com>
2023-05-14 22:31:09 -0400

    Create Part 3 - Starting with Questions - Question 4.sql

---
+++ Part 3 - Starting with Questions - Question 4.sql
@@ -0,0 +1,42 @@
+--What is the top-selling product from each city? 
+SELECT  sku,
+		productname,
+		city,
+		orderedquantity
+FROM (
+	SELECT
+		p.sku,
+		p.name as productname,
+		a.city,
+		SUM(orderedquantity) as orderedquantity,
+		ROW_NUMBER() OVER (PARTITION BY city ORDER BY SUM(orderedquantity) DESC) as rn
+	FROM allsessions as a
+	JOIN products as p
+	ON a.productsku=p.sku
+	WHERE city IS NOT null
+	GROUP BY p.sku, a.city, p.name
+	ORDER BY SUM(orderedquantity) DESC) as subquery
+WHERE rn = 1
+	
+--What is the top-selling product from each country? 
+SELECT  sku,
+		productname,
+		country,
+		orderedquantity
+FROM (
+	SELECT
+		p.sku,
+		p.name as productname,
+		a.country,
+		SUM(orderedquantity) as orderedquantity,
+		ROW_NUMBER() OVER (PARTITION BY country ORDER BY SUM(orderedquantity) DESC) as rn
+	FROM allsessions as a
+	JOIN products as p
+	ON a.productsku=p.sku
+	WHERE country IS NOT null
+	GROUP BY p.sku, a.country, p.name
+	ORDER BY SUM(orderedquantity) DESC) as subquery
+WHERE rn = 1
+
+
+--Can we find any pattern worthy of noting in the products sold?

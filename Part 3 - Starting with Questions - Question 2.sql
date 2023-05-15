ac0accc13bf99f90c538635a43977b4608946391
chloephuong <chloephuong09@gmail.com>
2023-05-14 22:31:38 -0400

    Update Part 3 - Starting with Questions - Question 2.sql

--- Part 3 - Starting with Questions - Question 2.sql
+++ Part 3 - Starting with Questions - Question 2.sql
@@ -1,7 +1,19 @@
-SELECT productsku, city,
+--What is the average number of products ordered from visitors in each city?
+SELECT  city,
 		ROUND(AVG(orderedquantity)) as avg_productordered
 FROM allsessions as a
 JOIN products as p
 ON a.productsku=p.sku
 WHERE city is not null
-GROUP BY city, productsku, orderedquantity
+GROUP BY city
+ORDER BY ROUND(AVG(orderedquantity)) DESC
+
+--What is the average number of products ordered from visitors in each country?
+SELECT  country,
+		ROUND(AVG(orderedquantity)) as avg_productordered
+FROM allsessions as a
+JOIN products as p
+ON a.productsku=p.sku
+WHERE country is not null
+GROUP BY country
+ORDER BY ROUND(AVG(orderedquantity)) DESC

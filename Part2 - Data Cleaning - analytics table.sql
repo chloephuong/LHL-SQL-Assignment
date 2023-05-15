81f072d7d583dc3da074b39c3c2f95ac5825e2ac
chloephuong <chloephuong09@gmail.com>
2023-05-15 13:59:27 -0400

    Update Part2 - Data Cleaning - analytics table.sql

--- Part2 - Data Cleaning - analytics table.sql
+++ Part2 - Data Cleaning - analytics table.sql
@@ -10,3 +10,7 @@ USING to_timestamp(visitstarttime, 'YYYY-MM-DD HH24:MI:SS');
 --Update unit price (unit cost) as it suggested should be divided by 1,000,000
 UPDATE analytics 
 SET unitprice = unitprice/1000000
+
+--Drop/Delete userid column because it offers no value
+ALTER TABLE analytics
+DROP COLUMN userid

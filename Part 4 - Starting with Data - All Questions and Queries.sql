8d6c142a1e097d6ee5b8b154ee25bfc0a403508d
chloephuong <chloephuong09@gmail.com>
2023-05-15 15:02:34 -0400

    Update Part 4 - Starting with Data - All Questions and Queries.sql

--- Part 4 - Starting with Data - All Questions and Queries.sql
+++ Part 4 - Starting with Data - All Questions and Queries.sql
@@ -8,9 +8,27 @@ HAVING COUNT(*) > 1;
 SELECT COUNT(DISTINCT fullvisitorid) AS unique_visitors
 FROM allsessions
 
---Find the total number of unique visitors by referring sites
+--Find the total number of unique visitors by channel
 SELECT channelgrouping,
 		COUNT(DISTINCT fullvisitorid) AS unique_visitors	
 FROM allsessions
 GROUP BY channelgrouping
 ORDER BY channelgrouping
+
+--Find each unique product viewed by each visitor
+SELECT DISTINCT ON (fullvisitorid, productsku) fullvisitorid, 
+	   productsku
+FROM (
+  SELECT fullvisitorid, 
+		 productsku, 
+		 MIN(pageviews) AS first_view_time
+  FROM allsessions
+  GROUP BY fullvisitorid, productsku
+) subquery
+ORDER BY fullvisitorid, productsku, first_view_time
+
+--Compute the percentage of visitors to the site that actually makes a purchase
+SELECT COUNT(DISTINCT fullvisitorid) AS total_visitors,
+       COUNT(DISTINCT CASE WHEN unitsold > 0 THEN fullvisitorid END) AS visitors_with_purchase,
+       100 * COUNT(DISTINCT CASE WHEN unitsold > 0 THEN fullvisitorid END) / COUNT(DISTINCT fullvisitorid) AS purchase_conversion_rate
+FROM analytics

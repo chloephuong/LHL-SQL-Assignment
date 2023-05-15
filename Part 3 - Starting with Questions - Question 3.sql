f3f768abaffe177f8c9c6cab0f36344cfb67c4dd
chloephuong <chloephuong09@gmail.com>
2023-05-15 16:58:12 -0400

    Create Part 3 - Starting with Questions - Question 3.sql

---
+++ Part 3 - Starting with Questions - Question 3.sql
@@ -0,0 +1,12 @@
+--Product categories of products ordered from visitors in each city and country?
+SELECT country, city, category
+FROM (
+	SELECT a.city, a.country, a.v2productcategory as category, p.orderedquantity
+	FROM allsessions AS a
+	JOIN products AS p
+	ON a.productsku=p.sku
+	WHERE orderedquantity > 0 ) AS subquery
+WHERE country IS NOT null 
+OR city IS NOT null
+GROUP BY country, city, category
+

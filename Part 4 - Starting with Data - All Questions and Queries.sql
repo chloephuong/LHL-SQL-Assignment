517133203529c7782ba009110f7a11617a60f339
chloephuong <chloephuong09@gmail.com>
2023-05-16 21:45:44 -0400

    Update Part 4 - Starting with Data - All Questions and Queries.sql

--- Part 4 - Starting with Data - All Questions and Queries.sql
+++ Part 4 - Starting with Data - All Questions and Queries.sql
@@ -1,21 +1,21 @@
---Find duplicate fullvisitorid records in allsessions table
+--Question 1: Find duplicate fullvisitorid records in allsessions table
 SELECT fullvisitorid, COUNT(*)
 FROM allsessions
 GROUP BY fullvisitorid
 HAVING COUNT(*) > 1
 
---Find the total number of unique visitors
+--Question 2: Find the total number of unique visitors
 SELECT COUNT(DISTINCT fullvisitorid) AS unique_visitors
 FROM allsessions
 
---Find the total number of unique visitors by channel
+--Question 3: Find the total number of unique visitors by channel
 SELECT channelgrouping,
 		COUNT(DISTINCT fullvisitorid) AS unique_visitors	
 FROM allsessions
 GROUP BY channelgrouping
-ORDER BY channelgrouping
+ORDER BY unique_visitors DESC
 
---Find each unique product viewed by each visitor
+--Question 4: Find each unique product viewed by each visitor
 SELECT DISTINCT ON (fullvisitorid, productsku) fullvisitorid, 
 	   productsku
 FROM (
@@ -27,7 +27,7 @@ FROM (
 ) subquery
 ORDER BY fullvisitorid, productsku, first_view_time
 
---Compute the percentage of visitors to the site that actually makes a purchase
+--Question 5: Compute the percentage of visitors to the site that actually makes a purchase
 SELECT COUNT(DISTINCT fullvisitorid) AS total_visitors,
        COUNT(DISTINCT CASE WHEN unitsold > 0 THEN fullvisitorid END) AS visitors_with_purchase,
        100 * COUNT(DISTINCT CASE WHEN unitsold > 0 THEN fullvisitorid END) / COUNT(DISTINCT fullvisitorid) AS purchase_conversion_rate
